6fd5c08fc7b6770437fb8dae6115b4e03ef939e5
vinz2112 <hippiefishmalta@gmail.com>
2023-03-26 12:35:52 +0200

    1st-initial commit-push

---
+++ app/src/main/java/com/example/fonefragmentviewmodel/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.fonefragmentviewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fonefragmentviewmodel.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
